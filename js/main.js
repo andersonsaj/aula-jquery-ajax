584e456fa04f074a160df3d8dd94b0d20318380f
andersonsaj <andersonsb09@hotmail.com>
2020-05-31 19:19:19 -0300

    utilizando bootstrap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCep(){
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/"+cep+"/json/";
     console.log(cep);
@@ -7,13 +8,20 @@ function consultaCep(){
         type:"GET",
         success: function(response){
             console.log(response);
-            document.getElementById("Cep").innerHTML = response.cep;
+           $("#CEP").html("cep" + response.cep);
            // document.getElementById("logradouro").innerHTML = response.logradouro;
            $("#logradouro").html(response.logradouro);
            document.getElementById("complemento").innerHTML = response.complemento;
             document.getElementById("bairro").innerHTML = response.bairro;
             document.getElementById("cidade").innerHTML = response.localidade;
-            document.getElementById("estado").innerHTML = response.uf;
+            document.getElementById("uf").innerHTML = response.uf;
+            $(".cep").show();
+            $(".barra-progresso").hide();
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
